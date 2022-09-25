c21669f32abeaa7aa648f5166aa6d0d878d694e7
Dhruv-Mehndhiratta <dmrrattaa7@gmail.com>
2022-09-25 11:01:21 +0530

    added axios

--- src/app/pages/Listing/index.tsx
+++ src/app/pages/Listing/index.tsx
@@ -1,4 +1,10 @@
+import { useEffect } from "react";
+import axios from 'axios';
+
 export const Listing = () => {
+  useEffect(() => {
+    axios.get('http://localhost:3004/tickets').then(res => console.log(res))
+  }, [])
   return <div className="app">
     Listing
   </div>;
