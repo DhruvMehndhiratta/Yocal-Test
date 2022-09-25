5162ee31ea38703c234cbe85c617d77c9c894fd2
Dhruv-Mehndhiratta <dmrrattaa7@gmail.com>
2022-09-25 11:47:10 +0530

    created custom card componment

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -1,9 +1,20 @@
 import { FC } from "react";
 import { Ticket } from "../../types";
+
+import { Button, Card as CardComponent } from "react-bootstrap";
 interface CardProps {
   ticket: Ticket;
 }
 export const Card: FC<CardProps> = ({ ticket }) => {
-  console.log(ticket);
-  return <div className="app">Card</div>;
+  return (
+    <CardComponent className="card-component">
+      <CardComponent.Body>
+        <CardComponent.Title>{`Status: ${ticket.status}`}</CardComponent.Title>
+        <CardComponent.Text>
+          {`Ticket Number: ${ticket.number}`}
+        </CardComponent.Text>
+        <Button variant="primary">Go somewhere</Button>
+      </CardComponent.Body>
+    </CardComponent>
+  );
 };
