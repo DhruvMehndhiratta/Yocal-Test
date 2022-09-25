f38e2bf3ef75bf9e2702395c23086d4567320929
Dhruv-Mehndhiratta <dmrrattaa7@gmail.com>
2022-09-25 11:23:43 +0530

    added user and ticked mapped response

--- src/app/pages/Listing/index.tsx
+++ src/app/pages/Listing/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
 import { API_BASE_URL } from '../../../constants';
-
+import { User, Ticket } from '../../types/'
 
 
 export const Listing = () => {
@@ -17,13 +17,26 @@ export const Listing = () => {
 
   useEffect(() => {
     (async () => {
-      const users = await fetchUsers();
-      let response = await fetchTickets();
-      
+      Promise.all([fetchUsers(), fetchTickets()]).then((values) => {
+  
+        const [users, ticketResponse] = values;
+        
+        ticketResponse.data.forEach((item: Ticket) => {
+          const userIndex = users.data.findIndex((val: User) => item.userId === val.id)
+          if(userIndex !== -1){
+            item['user'] = users.data[userIndex];
+          }
+        })
+        setTickets(ticketResponse.data)
+      })
+      // const users = await fetchUsers();
+      // let response = await fetchTickets();
+      // response.forEach((item) => )
     })();
 
 
   }, [])
+  console.log(tickets, "tickets")
   return <div className="app">
     Listing
   </div>;
--- src/app/types/ticket.ts
+++ src/app/types/ticket.ts
@@ -1,6 +1,8 @@
+import { User } from './user'
 export type Ticket = {
 	id: string;
 	userId: number;
 	number: string;
 	status: string;
+	user?: User;
 }
