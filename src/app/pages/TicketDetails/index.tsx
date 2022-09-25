e393add5ce26ae840f9131e1a55bc530d6e44408
Dhruv-Mehndhiratta <dmrrattaa7@gmail.com>
2022-09-25 01:59:17 -0400

    Worked on adding details to ticket and added loading state

--- src/app/pages/TicketDetails/index.tsx
+++ src/app/pages/TicketDetails/index.tsx
@@ -1,27 +1,50 @@
 import axios, { AxiosResponse } from 'axios';
 import { useEffect, useState } from 'react';
+import { Card } from 'react-bootstrap';
 import { useParams } from 'react-router';
-import { Ticket } from 'src/app/types';
+import { Ticket, User } from 'src/app/types';
 import { API_BASE_URL } from 'src/constants';
+
+type RouteParams = { ticketId: string; };
 
 export const TicketDetails = () => {
 
-  const [ticket, setTicket] = useState<Ticket | null>(null);
+  const [ ticket, setTicket ] = useState<Ticket | null>(null);
+  const [ user, setUser ] = useState<User | null>(null);
+  const [ loading, setLoading ] = useState(false);
 
-  const params = useParams<any>();
+  const params = useParams<RouteParams>();
   const { ticketId='' } = params;
 
   useEffect(() => {
+    const fetchUser = async (userId: string) => {
+      if(userId) {
+        const response: AxiosResponse = await axios.get(`${API_BASE_URL}/users/${userId}`);
+        setUser(response.data);
+        setLoading(false);
+      }
+    }
+
     const fetchTicket = async () => {
       const response: AxiosResponse = await axios.get(`${API_BASE_URL}/tickets/${ticketId}`);
       setTicket(response.data);
+      fetchUser(response.data.userId);
     }
     fetchTicket();
-  }, [ ticketId ])
+  }, [ ticketId ]);
 
-  console.log(ticket, "ticekt");
+  if(loading) {
+    return null;
+  }
 
-  return <div className="app">
-    TicketDetails
+  const { id='', status='', number='' } = ticket || {};
+
+  return <div>
+    <Card>
+      <Card.Body>
+        <p>Ticket No.: {number}</p>
+        <p className='text-capitalize'>Status: {status}</p>
+      </Card.Body>
+    </Card>
   </div>;
 };
